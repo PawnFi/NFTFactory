79f01c5244ac9aeaff4e157b504d1d8207b60095
dirk-pawnfi <wenwei.xu@pawnfi.io>
2023-03-23 17:03:19 +0800

    delete unused interface file: CreateProxyFactory.sol

--- contracts/interfaces/CreateProxyFactory.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.0;
-
-interface CreateProxyFactory {
-    
-    function deploy(address logic, address admin, bytes memory data, bytes32 salt) external returns(address proxy);
-
-    function computeAddress(bytes32 salt) external view returns (address);
-}
